3b562e43027374c3eceb85add1cd18b0c6bab7c5
Jenny Gee <themainguru@gmail.com>
2012-06-15 11:09:00 -0400

    update

---
+++ api/html5_fileLocalFileSystem.js
@@ -0,0 +1,91 @@
+/*
+* Copyright 2010-2012 Research In Motion Limited.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+/**
+ * @toc {File} HTML5 LocalFileSystem
+ * @class This object provides a way to obtain root file systems.
+ * @constructor
+ * @description This object provides a way to obtain root file systems.
+*/
+LocalFileSystem = function() {};
+
+    /**
+    * @description  The postMessage() method on Web Workers objects invoked the method of the same name on the port, with the same arguments, and returned the same return value.
+    * @param {Object} message A message to post when worker is created.
+    * @PB10+
+    * @RIPPLE
+	* @BB10X
+    * @example
+    * Main script:
+    *
+    * &lt;script type="text/javascript"&gt;
+    *
+    * &lt;/script&gt;
+    *
+    *
+    */
+    LocalFileSystem.prototype.requestFileSystem  = function() {};
+
+LocalFileSystem = {
+		/**
+		 * @constant
+		 * @type String
+		 * @description Localstorage that will persists after application has been completed.
+		 */
+		PERSISTENT: PERSISTENT,
+
+		/**
+		 * @constant
+		 * @type String
+		 * @description Localstorage that is temporary
+		 */
+		TEMPORARY: TEMPORARY,
+	};	
+
+    /**
+    * @description  Requests the filesystem. 
+    * @param 
+    * @PB10+
+    * @RIPPLE
+	* @BB10X
+    * @example
+    * Main script:
+    *
+    * &lt;script type="text/javascript"&gt;
+    *
+    * &lt;/script&gt;
+    *
+    */
+    LocalFileSystem.prototype.requestFileSystem  = function() {};
+
+	
+    /**
+    * @description  Requests the DirectoryEntry or FileEntry Object using local URI. 
+    * @param {Object} message A message to post when worker is created.
+    * @PB10+
+    * @RIPPLE
+	* @BB10X
+    * @example
+    * Main script:
+    *
+    * &lt;script type="text/javascript"&gt;
+    *
+    * &lt;/script&gt;
+    *
+    *
+    */
+    LocalFileSystem.prototype.resolveLocalFileSystemURI  = function() {};
+	
---
+++ api/html5_fileMetadata.js
@@ -0,0 +1,50 @@
+/*
+* Copyright 2010-2012 Research In Motion Limited.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+/**
+ * @toc {File} HTML5 Metadata
+ * @class Object supplies information regarding to the state of file or directory
+ * @constructor
+ * @description This object provides some information regardignto the state of the file or directory object
+*/
+Metadata = function() {};
+
+    /**
+    * @description  The postMessage() method on Web Workers objects invoked the method of the same name on the port, with the same arguments, and returned the same return value.
+    * @param {Object} message A message to post when worker is created.
+    * @PB10+
+    * @RIPPLE
+	* @BB10X
+    * @example
+    * Main script:
+    *
+    * &lt;script type="text/javascript"&gt;
+    *
+    * &lt;/script&gt;
+    *
+    *
+    */
+    Metadata.prototype.getMetadata  = function() {};
+   
+    /**
+     * @field
+     * @description The name of the file
+     * @type File
+     * @PB10+
+     * @RIPPLE
+	 * @BB10X
+     */
+    Metadata.modificationTime = undefined;
