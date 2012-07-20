38823c0c2335d4972f1f3b270d27e2c0935d6fb8
Rowell Cruz <rcruz@rim.com>
2012-07-20 18:05:32 -0400

    Removed windows line breaks from contextmenu API

--- api/blackberry_ui_contextmenu.js
+++ api/blackberry_ui_contextmenu.js
@@ -66,6 +66,7 @@ blackberry.ui.contextmenu = {
  * @param {String} label A string that will be displayed to the user describing the custom context to be performed. ie: Edit
  * @param {String} icon A path to the an image to display in the context menu
  * @BB10X
+ * @RIPPLE
  * @example
  * &lt;script type="text/javascript"&gt;
  *
@@ -84,6 +85,7 @@ blackberry.ui.contextmenu = {
  * @param {String[]} contexts An array of constants defining which contexts this new item should appear.
  * @param {String} actionId An id that uniquely defines the action to be removed from the context menu.
  * @BB10X
+ * @RIPPLE
  * @example
  * &lt;script type="text/javascript"&gt;
  *
